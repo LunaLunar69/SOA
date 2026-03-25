eceb9059d0574c70872f5e1eac29ae7498d65dce
MiguelEnok <miguelenokjuarezochoa@gmail.com>
2026-03-25 12:03:24 -0600

    Hotfix Current_Input_A value added to parquet generator

--- parquetGenerator.py
+++ parquetGenerator.py
@@ -56,6 +56,8 @@ def run_massive_generator():
             longitud_minima = min(len(I_trim), len(P_out_trim), len(sym2), len(P_out_trim2))
             
             df = pd.DataFrame({
+                'Current_Input_A': I_trim[:longitud_minima],
+
                 # Señales Nuevas de SOA5 Recortadas
                 'Input_Sym2': sym2[:longitud_minima],
                 'Output_P2': P_out_trim2[:longitud_minima] * 1e3,
