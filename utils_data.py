7ef63aba20fa81c3199a7386bfea4d191e907bf9
aguazul220604 <garciaguazul12@gmail.com>
2026-04-06 12:51:18 -0600

    tight code for training

--- utils_data.py
+++ utils_data.py
@@ -7,7 +7,7 @@ import glob
 import json
 from numpy.lib.stride_tricks import sliding_window_view
 
-FEATURES = ['Output_P2', 'Bit_Rate', 'Beta_RC', 'Rango_Corr']
+FEATURES = ['Time', 'Output_P2', 'Bit_Rate', 'Beta_RC', 'Rango_Corr']
 TARGET = ['Input_Sym2']
 ALL_COLUMNS = FEATURES + TARGET
 
